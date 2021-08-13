3e74637c5b93bf60f4ea2125905a845769d2fb93
MD Ikramul Kayes <ikramul.kayesgg@gmail.com>
2021-08-13 11:55:57 +0600

    handling multiple parameter in a function

--- practice1.py
+++ practice1.py
@@ -1,4 +1,4 @@
-def gg(*kaka):
+def gg(*kaka):       # *kaka is a tuple
     k = "I am the first " + str(kaka[0])
     return k
-print(gg("Mami","chacha","mahi"))
+print(gg("Mami","chacha","mahi")) 
