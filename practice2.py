756239f53b5c5cd056ece9a656ac4736d8e50dd4
MD Ikramul Kayes <ikramul.kayesgg@gmail.com>
2021-08-13 12:59:43 +0600

    handling kwargs defult values in a function

---
+++ practice2.py
@@ -0,0 +1,14 @@
+def child(c1,c2,c3):
+    print(c1)
+    print(c2)
+    print(c3)
+child(c3 = "gaga",c1="Kid",c2 = "Drake")
+def car(**kaka): #passing dictinary in a function hell ye
+    print("Model is",kaka["model"])
+car(no=121313,model="Lambo")
+def sumkk(a,b=2): #defult value process
+    return(a**2+b)
+print(sumkk(2,3))
+def em(gaga):    #empty function
+    pass
+print(em)
