a9975afa84aa7ab2c0297a75b0676dc69cc01c22
MD Ikramul Kayes <ikramul.kayesgg@gmail.com>
2021-08-13 12:03:16 +0600

    handling multiple variable in a function

--- practice1.py
+++ practice1.py
@@ -2,3 +2,7 @@ def gg(*kaka):       # *kaka is a tuple
     k = "I am the first " + str(kaka[0])
     return k
 print(gg("Mami","chacha","mahi")) 
+def ad(mama, *jj): #ezy life
+    print(mama)
+    print(jj)
+ad("cod","ac","fc")
