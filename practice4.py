3dbec0ce3e489ca467ef6f25c5afb2e256e639bd
MD Ikramul Kayes <67923321+ikramulkayes@users.noreply.github.com>
2021-08-14 13:14:05 +0600

    Delete practice4.py

--- practice4.py
+++
@@ -1,8 +0,0 @@
-def function_name(days):
-    years = days//365
-    remain = days%365
-    months = remain // 30
-    remain = remain % 30
-    print(years,"years,",months,"months and",remain,"days")
-num1 = int(input("Enter a number: "))
-function_name(num1)    
