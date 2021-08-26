ac10e84a7f04eac7ffec489e31dab23646137277
MD Ikramul Kayes <md.ikramul.kayes@g.bracu.ac.bd>
2021-08-26 10:34:33 +0600

    section sort

---
+++ practice28.py
@@ -0,0 +1,10 @@
+lst = [2,53,-2,6]
+for i in range(len(lst)):
+    min_index = i
+    for j in range(i+1,len(lst)): #section sort 
+        if lst[j] < lst[i]:
+            min_index = j
+    temp = lst[min_index]
+    lst[min_index] = lst[i]
+    lst[i] = temp
+print(lst)
