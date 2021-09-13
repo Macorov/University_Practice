cd382c90a72af52776399a8350cd016bc1cf823f
MD Ikramul Kayes <md.ikramul.kayes@g.bracu.ac.bd>
2021-09-13 18:14:26 +0600

    Create practice53.py

---
+++ practice53.py
@@ -0,0 +1,20 @@
+file = open("task1.txt")
+lst = file.readlines()
+
+maxlen = None
+maxword = None
+count = 0
+for elm in lst:
+    elm = elm.strip()
+    elm = elm.split()
+    
+    for val in elm:
+        
+        if count == 0:
+            maxlen = len(val)
+            maxword = val
+            count += 1
+        elif len(val) > maxlen:
+            maxlen = len(val)
+            maxword = val
+print(maxword)
